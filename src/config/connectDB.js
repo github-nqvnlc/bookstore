9473c48cb8d6903e878d889dad10804f53c68e6d
github.nqvnlc <locnvgcd18350@fpt.edu.vn>
2022-08-02 23:46:45 +0700

    connect MySQL Database

---
+++ src/config/connectDB.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("user", "root", null, {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+let connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("MySQL Database connect successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+module.exports = connectDB;
