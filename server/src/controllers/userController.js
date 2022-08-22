e431e494d53ea5b577800028ce68c1d8509ced83
github-nqvnlc <locnv14@gmail.com>
2022-08-22 20:40:33 +0700

    get all user

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -20,6 +20,18 @@ let handleLogin = async (req, res) => {
   });
 };
 
+let handleGetAllUsers = async (req, res) => {
+  let id = req.body.id; //ALL, SINGLE
+  let users = await userService.getAllUsers(id);
+
+  return res.status(200).json({
+    errCode: 0,
+    errMessage: "oke",
+    users
+  })
+}
+
 module.exports = {
   handleLogin: handleLogin,
+  handleGetAllUsers: handleGetAllUsers
 };
